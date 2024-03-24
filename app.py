bac7892e47404eb936bc49b26869319a74ee070d
sreedhar-98 <sreedhardronamraju1@gmail.com>
2024-03-24 16:38:26 +0530

    Final Commit

--- app.py
+++ app.py
@@ -4,18 +4,24 @@ from callback import callback
 from src.face_detector import FaceDetector
 from twilio.rest import Client
 
-account_sid=st.secrets["sid"]
-auth_token=st.secrets["token"]
-client=Client(account_sid,auth_token)
 
-@st.cache_resource
+@st.cache_resource()
 def get_detector():
     return FaceDetector(model='model/public/ultra-lightweight-face-detection-rfb-320/FP32/ultra-lightweight-face-detection-rfb-320.xml',
                             confidence_thr=0.8,
                             overlap_thr=0.3)
 
+@st.cache_data(ttl=86400)
+def get_twilio_token():
+    account_sid=st.secrets["sid"]
+    auth_token=st.secrets["token"]
+    client=Client(account_sid,auth_token)
+    token=client.tokens.create()
+    return token
 
-token=client.tokens.create()
+
+token=get_twilio_token()
+
 st.title("Person Counter App")
 
 show_metrics = st.checkbox("Show FPS and Latency", value=False)
@@ -24,4 +30,4 @@ detector=get_detector()
 
 
 webrtc_streamer(key="sample", video_frame_callback=lambda frame:callback(frame,detector,show_metrics),media_stream_constraints={"video":True,"audio":False},
-                rtc_configuration={"iceServers":token.ice_servers})
+                 rtc_configuration={"iceServers":token.ice_servers})
